f154b8e789e06e5b0a8e1304e96920c68021bd8b
Takayuki Maruyama <bis5.wsys@gmail.com>
2017-12-19 19:20:38 +0900

    init spring boot project

---
+++ src/main/java/net/bis5/mattermost/simplelock/MattermostSimpleLockApplication.java
@@ -0,0 +1,12 @@
+package net.bis5.mattermost.simplelock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MattermostSimpleLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MattermostSimpleLockApplication.class, args);
+	}
+}
